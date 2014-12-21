2dea70b6ba27981096805db2df5eb29b059e921b
Nils Andreas Svee <nils.a.svee@gmail.com>
2014-12-21 23:31:46 +0100

    Embed Iconic ZIP dll into executable

---
+++ SolderHelper/Core.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Reflection
+Imports System.IO
+
+Public Module Core
+
+    Private _initialized As Boolean
+
+    Public Sub EnsureInitialized()
+        If Not _initialized Then
+            AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf AssemblyResolve
+            _initialized = True
+        End If
+    End Sub
+
+    Private Function AssemblyResolve(ByVal sender As Object, ByVal e As ResolveEventArgs) As Assembly
+        Dim resourceFullName As String = "Ionic.Zip.dll"
+
+        Dim thisAssembly As Assembly = Assembly.GetExecutingAssembly()
+        Using resource As Stream = thisAssembly.GetManifestResourceStream(resourceFullName)
+            If resource IsNot Nothing Then Return Assembly.Load(ToBytes(resource))
+            Return Nothing
+        End Using
+    End Function
+
+    Private Function ToBytes(ByVal instance As Stream) As Byte()
+        Dim capacity As Integer = If(instance.CanSeek, Convert.ToInt32(instance.Length), 0)
+
+        Using result As New MemoryStream(capacity)
+            Dim readLength As Integer
+            Dim buffer(4096) As Byte
+
+            Do
+                readLength = instance.Read(buffer, 0, buffer.Length)
+                result.Write(buffer, 0, readLength)
+            Loop While readLength > 0
+
+            Return result.ToArray()
+        End Using
+    End Function
+
+End Module
--- SolderHelper/My Project/AssemblyInfo.vb
+++ SolderHelper/My Project/AssemblyInfo.vb
@@ -1,4 +1,6 @@
-﻿Imports System
+﻿Imports System.Resources
+
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -12,10 +14,10 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("SolderHelper")> 
-<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyCopyright("Copyright © Nils Andreas Svee 2014")> 
 <Assembly: AssemblyTrademark("")> 
 
-<Assembly: ComVisible(False)>
+<Assembly: ComVisible(False)> 
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("d6d7b574-e8e8-4ab9-9a81-8d7bd7b1c359")> 
@@ -31,5 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.1.0.0")> 
+<Assembly: AssemblyFileVersion("0.1.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
